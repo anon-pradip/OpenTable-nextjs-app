601d80f558ca60df14a2244fd366c4bd600c39bf
anon-pradip <pradipsubedi360@gmail.com>
2023-02-24 22:08:50 +0545

    make menu page dynamic

--- app/restaurant/[slug]/menu/components/BodySection.tsx
+++ app/restaurant/[slug]/menu/components/BodySection.tsx
@@ -1,22 +1,38 @@
 import React from "react";
 import Link from "next/link";
+import { Item } from "@prisma/client";
 
-const BodySection = () => {
+const BodySection = ({ slug, menu }: { slug: string; menu: Item[] }) => {
   return (
     <div className=" flex justify-center items-center">
       <div className="rounded-sm flex flex-col space-y-4 px-1 py-3 bg-white shadow-[rgba(0,_0,_0,_0.24)_0px_3px_8px] max-w-2xl mt-4 md:-mt-10 w-full">
         {/* overview/menu */}
         <div className="flex space-x-5">
           <Link href="/restaurant/cozy5stars">Overview</Link>
-          <Link href="/restaurant/cozy5stars/menu">Menu</Link>
+          <Link href={`/restaurant/${slug}/menu`}>Menu</Link>
         </div>
         <hr />
         <h1 className=" text-2xl font-bold">Menu</h1>
-        <div className=" px-2 py-2 border max-w-md">
-          <p className=" font-bold">Surf and Turf</p>
-          <p>blah blah</p>
-          <p className=" font-bold mt-4">$60.00</p>
-        </div>
+        {menu.length ? (
+          <div className=" flex items-center justify-center gap-3 flex-wrap">
+            {menu.map((item) => {
+              return (
+                <div
+                  key={item.id}
+                  className="flex flex-col space-y-3 px-2 border border-gray-700 rounded-sm w-80"
+                >
+                  <p className=" text-lg font-bold">{item.name}</p>
+                  <p className=" text-gray-800">{item.description}</p>
+                  <p className=" font-bold text-lg mt-3">{item.price}</p>
+                </div>
+              );
+            })}
+          </div>
+        ) : (
+          <p className="text-3xl font-bold">
+            This restaurant doesn't have any menu
+          </p>
+        )}
       </div>
     </div>
   );
--- app/restaurant/[slug]/menu/page.tsx
+++ app/restaurant/[slug]/menu/page.tsx
@@ -1,16 +1,33 @@
-import Link from "next/link";
-import React from "react";
 import HeroSection from "../components/HeroSection";
 import BodySection from "./components/BodySection";
+import { PrismaClient } from "@prisma/client";
 
-const ResataurantMenu = () => {
+const prisma = new PrismaClient();
+
+const findItems = async (slug: string) => {
+  const restaurant = await prisma.restaurant.findUnique({
+    where: {
+      slug,
+    },
+    select: {
+      items: true,
+    },
+  });
+  if (!restaurant) {
+    throw new Error("cannot find  items");
+  }
+  return restaurant.items;
+};
+
+const ResataurantMenu = async ({ params }: { params: { slug: string } }) => {
+  const menu = await findItems(params.slug);
   return (
     <div className=" flex flex-col pb-11">
       {/* Hero section */}
       <HeroSection />
 
       {/* body section */}
-      <BodySection />
+      <BodySection slug={params.slug} menu={menu} />
     </div>
   );
 };
