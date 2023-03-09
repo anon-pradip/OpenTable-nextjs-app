9954aae89226f49753cdee85e5bc7de53d333236
anon-pradip <pradipsubedi360@gmail.com>
2023-03-09 22:18:29 +0545

    fix validation file error

--- pages/api/auth/signup.ts
+++ pages/api/auth/signup.ts
@@ -3,21 +3,54 @@ import type { NextApiRequest, NextApiResponse } from "next";
 import validator from "validator";
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  const errors: string[] = [];
+
   if (req.method === "POST") {
-    const { firstName, lastName, email, city, password } = req.body;
+    const { first_name, email, last_name, city, password } = req.body;
 
     const validationSchema = [
       {
-        valid: validator.isLength(firstName, {
+        valid: validator.isLength(first_name, {
           min: 1,
           max: 20,
         }),
         errorMessage: "First Name is invalid",
       },
+      {
+        valid: validator.isLength(last_name, {
+          min: 1,
+          max: 20,
+        }),
+        errorMessage: "Last Name is invalid",
+      },
+      {
+        valid: validator.isEmail(email),
+        errorMessage: "Email is invalid",
+      },
+      {
+        valid: validator.isLength(city, {
+          min: 1,
+        }),
+        errorMessage: "City is invalid",
+      },
+      {
+        valid: validator.isStrongPassword(password),
+        errorMessage: "Password is not strong enough",
+      },
     ];
 
+    validationSchema.forEach((check) => {
+      if (!check.valid) {
+        errors.push(check.errorMessage);
+      }
+    });
+
+    if (errors.length) {
+      return res.status(400).json({ errorMessage: errors[0] });
+    }
+
     res.status(200).json({
-      hello: "body",
+      hello: "hello",
     });
   }
 }
