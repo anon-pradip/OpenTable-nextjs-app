a247b3ff18a8f1dd93cd6323195db7bf4894e19a
anon-pradip <pradipsubedi360@gmail.com>
2023-03-09 22:40:47 +0545

    make bcrypt file

--- pages/api/auth/signup.ts
+++ pages/api/auth/signup.ts
@@ -1,8 +1,13 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { PrismaClient } from "@prisma/client";
 import type { NextApiRequest, NextApiResponse } from "next";
 import validator from "validator";
 
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
+const prisma = new PrismaClient();
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
   const errors: string[] = [];
 
   if (req.method === "POST") {
@@ -48,6 +53,10 @@ export default function handler(req: NextApiRequest, res: NextApiResponse) {
     if (errors.length) {
       return res.status(400).json({ errorMessage: errors[0] });
     }
+
+    const userWithEmail = await prisma.user.findUnique({
+      where: {},
+    });
 
     res.status(200).json({
       hello: "hello",
